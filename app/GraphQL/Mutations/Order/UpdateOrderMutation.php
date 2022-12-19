5242fbf0f5ad1928c7b6ab36209f8adeead9f4ac
AurangzaibRepository <aurangzaib_najam@hotmail.com>
2022-12-19 16:51:42 +0500

    Specifies attribuets adn ype

--- app/GraphQL/Mutations/Order/UpdateOrderMutation.php
+++ app/GraphQL/Mutations/Order/UpdateOrderMutation.php
@@ -6,3 +6,16 @@ use App\Models\Order;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Mutation;
 use Rebing\GraphQL\Support\Facades\GraphQL;
+
+class UpdateOrderMutation extends Mutation
+{
+    protected $attributes = [
+        'name' => 'UpdateOrder',
+        'description' => 'Update order',
+    ];
+
+    public function type(): Type
+    {
+        return GraphQL::type('Order');
+    }
+}
