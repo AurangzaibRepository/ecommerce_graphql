17e9c3a07bdf3afe3d94c64f47e789da1a62599c
AurangzaibRepository <aurangzaib_najam@hotmail.com>
2022-12-19 19:07:42 +0500

    Sepcife attributes and type

--- app/GraphQL/Mutations/Order/DeleteOrderMutation.php
+++ app/GraphQL/Mutations/Order/DeleteOrderMutation.php
@@ -6,3 +6,16 @@ use App\Models\Order;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Mutation;
 use Rebing\GraphQL\Support\Facades\GraphQL;
+
+class DeleteOrderMutation extends Mutation
+{
+    protected $attributes = [
+        'name' => 'DeleteOrder',
+        'description' => 'Delete order',
+    ];
+
+    public function type(): Type
+    {
+        return Type::string();
+    }
+}
