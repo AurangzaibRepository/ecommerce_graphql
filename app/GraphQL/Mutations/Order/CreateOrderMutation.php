48e2007ddd425d05d6fffd7f5e0a42c691cdce95
AurangzaibRepository <aurangzaib_najam@hotmail.com>
2022-12-19 15:19:31 +0500

    Specifies arguments

--- app/GraphQL/Mutations/Order/CreateOrderMutation.php
+++ app/GraphQL/Mutations/Order/CreateOrderMutation.php
@@ -18,4 +18,25 @@ class CreateOrderMutation extends Mutation
     {
         return GraphQL::type('Order');
     }
+
+    public function args(): array
+    {
+        return [
+            'product_count' => [
+                'name' => 'product_count',
+                'type' => Type::int(),
+                'rules' => 'required',
+            ],
+            'total_price' => [
+                'name' => 'total_price',
+                'type' => Type::float(),
+                'rules' => 'required',
+            ],
+            'user_id' => [
+                'name' => 'user_id',
+                'type' => Type::int(),
+                'rules' => 'required|exists:users,id',
+            ],
+        ];
+    }
 }
