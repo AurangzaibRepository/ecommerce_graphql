2dad8e282e7218fe175bb3239f281a302e5f337b
AurangzaibRepository <aurangzaib_najam@hotmail.com>
2022-12-19 15:15:36 +0500

    Specifies attributes and type

--- app/GraphQL/Mutations/Order/CreateOrderMutation.php
+++ app/GraphQL/Mutations/Order/CreateOrderMutation.php
@@ -6,3 +6,16 @@ use App\Models\Order;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Mutation;
 use Rebing\GraphQL\Support\Facades\GraphQL;
+
+class CreateOrderMutation extends Mutation
+{
+    protected $attributes = [
+        'name' => 'CreateOrder',
+        'description' => 'Create order',
+    ];
+
+    public function type(): Type
+    {
+        return GraphQL::type('Order');
+    }
+}
