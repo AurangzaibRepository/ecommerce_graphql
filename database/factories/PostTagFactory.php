e23ba34a44cbdca0ab1a003776988b10338ca197
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-12-20 15:46:35 +0500

    Creates post tag factory class

---
+++ database/factories/PostTagFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\PostTag;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\PostTag>
+ */
+class PostTagFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
