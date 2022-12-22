d41111867c173975af577e65e29803ac724daab2
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-12-22 12:48:57 +0500

    Specifies attributes in factory

--- database/factories/OrderProductFactory.php
+++ database/factories/OrderProductFactory.php
@@ -5,16 +5,23 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Order;
 use App\Models\Product;
+use App\Models\OrderProduct;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderProduct>
  */
 class OrderProductFactory extends Factory
 {
-    public function definition()
+    protected $model = OrderProduct::class;
+
+    public function definition(): array
     {
+        $orderIdList = Order::all()->pluck('id');
+        $productIdList = Product::all()->pluck('id');
+
         return [
-            //
+            'order_id' => $this->faker->randomElement($orderIdList),
+            'product_id' => $this->faker->randomElement($productIdList),
         ];
     }
 }
