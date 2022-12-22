759eb5097458aeef46afed11879d806bf52405fa
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-12-22 12:43:38 +0500

    Cretes order product factory

---
+++ database/factories/OrderProductFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Order;
+use App\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderProduct>
+ */
+class OrderProductFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
