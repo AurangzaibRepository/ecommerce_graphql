971d4cbcba82d0cb27247ed32d0b0b95a40c45b0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-12-22 12:54:13 +0500

    Specifies seeder funcion

--- database/seeders/OrderProductSeeder.php
+++ database/seeders/OrderProductSeeder.php
@@ -4,10 +4,13 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\OrderProduct;
 
 class OrderProductSeeder extends Seeder
 {
     public function run(): void
     {
+        OrderProduct::factory(20)
+                     ->create();
     }
 }
