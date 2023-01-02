6838b29fc9efef243fe140f71735bc395b570937
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-03 00:52:26 +0500

    Specifies packages and namespace

---
+++ app/GraphQL/Mutations/Auth/Login.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\GraphQL\Mutations\Auth;
+
+use GraphQL\Type\Definiton\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Mutation;
+
+class Login extends Mutation
+{
+}
