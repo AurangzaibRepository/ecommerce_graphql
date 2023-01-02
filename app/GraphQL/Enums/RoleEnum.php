610b04b57c5b20da069a23492ccbaa9cf063a00d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-02 22:32:30 +0500

    Specifies attributes

--- app/GraphQL/Enums/RoleEnum.php
+++ app/GraphQL/Enums/RoleEnum.php
@@ -6,5 +6,12 @@ use Rebing\GraphQL\Support\EnumType;
 
 class RoleEnum extends EnumType
 {
-    
+    protected $attributes = [
+        'name' => 'role',
+        'description' => 'List of user roles',
+        'values' => [
+            'Admin' => 'Admin',
+            'User' => 'User',
+        ],
+    ];
 }
