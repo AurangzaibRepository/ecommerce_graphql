81b4fdd70ead287440b63b6cff981f2b229fc31e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-03 00:03:59 +0500

    Amends name value

--- app/GraphQL/Enums/RoleEnum.php
+++ app/GraphQL/Enums/RoleEnum.php
@@ -7,7 +7,7 @@ use Rebing\GraphQL\Support\EnumType;
 class RoleEnum extends EnumType
 {
     protected $attributes = [
-        'name' => 'role',
+        'name' => 'RoleEnum',
         'description' => 'List of user roles',
         'values' => [
             'Admin' => 'Admin',
