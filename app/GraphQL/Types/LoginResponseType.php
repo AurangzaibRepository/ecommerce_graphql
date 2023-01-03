42854034faf56e63067ff77f94f6154179122053
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-03 15:36:04 +0500

    Includes packages and namespace

---
+++ app/GraphQL/Types/LoginResponseType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\GraphQL\Types;
+
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+
+class LoginResponseType extends GraphQLType 
+{
+}
