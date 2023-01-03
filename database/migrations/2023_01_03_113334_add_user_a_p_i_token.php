bac6c02bc1f1d84eebb46e913ac07f1bc00d93d1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-01-03 16:40:30 +0500

    Adds api toekn field

--- database/migrations/2023_01_03_113334_add_user_a_p_i_token.php
+++ database/migrations/2023_01_03_113334_add_user_a_p_i_token.php
@@ -8,5 +8,11 @@ return new class extends Migration
 {
     public function up(): void
     {
+        Schema::table('users', function($table) {
+            $table->string('api_token', 80)->after('role')
+                ->unique()
+                ->nullable()
+                ->default(null);
+        });
     }
 };
